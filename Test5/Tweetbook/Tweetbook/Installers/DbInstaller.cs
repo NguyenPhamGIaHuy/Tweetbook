95f51f0593a1214a2de27bfc8c414bd1e6deaf22
NguyenPhamGIaHuy <nguyenphamgiahuy01@gmail.com>
2021-06-20 23:45:14 +0700

    Retrieving resources with GET

---
+++ Test5/Tweetbook/Tweetbook/Installers/DbInstaller.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tweetbook.Data;
+using Tweetbook.Services;
+
+namespace Tweetbok.Installers
+{
+    public class DbInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration Configuration)
+        {
+            services.AddDbContext<DataContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<DataContext>();
+            services.AddControllersWithViews();
+
+            services.AddSingleton<IPostService, PostService>();
+        }
+
+        public void InstallServices(IServiceCollection services, object configuration)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
